114186fdba571f2e003be20b270170656331916c
nserain <nicolas.serain@hetic.net>
2015-02-10 15:32:15 +0100

    Connexion
    html et php pour se connecter via ses logs

---
+++ php/connexion.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas
+ * Date: 10/02/2015
+ * Time: 13:48
+ */
+require('connect.php');
+if(isset($_POST["name"]) && isset($_POST['pass1'])){
+    $sql="SELECT idUser FROM users WHERE nameUser='".$_POST["name"]."' and password='".$_POST["pass1"]."';";
+    $resultats=$db->query($sql);
+    $resultats->setFetchMode(PDO::FETCH_OBJ);
+    $resultat = $resultats->fetch();
+
+
+
+    if($resultats){
+        $id=$resultat->idUser;
+        $sql="SELECT nameCharacter from characters where idUSer=$resultat->idUser;";
+        $resultats=$db->query($sql);
+        $resultats->setFetchMode(PDO::FETCH_OBJ);
+
+        while( $resultat = $resultats->fetch() )
+        {
+            echo "Bienvenue ".$resultat->nameCharacter." ! ";
+        }
+
+    }
+    else
+    {
+        echo "Ce compte n'existe pas encore";
+    }
+
+} else
+{
+    echo "Veuillez réessayer";
+}
