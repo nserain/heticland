256259e5c2120cc54a7ec10e99876b7f722010e0
nserain <nicolas.serain@hetic.net>
2015-02-12 15:24:44 +0100

    encore une fusion

--- duel.php
+++ duel.php
@@ -1,3 +1,4 @@
+<?php include('php/connexion.php'); ?>
 <!DOCTYPE html>
 <html land="fr">
 <head>
@@ -13,31 +14,42 @@
 
 <body>
 
+    <div class="salledeclasse">
 		<div class="infos">
-
 			<table>	
 				<tbody>
+				<thead><h2>Bonjour <?php echo $_SESSION['nameCharacter']; ?></h2></thead>
 					<tr>
-						<td rowspan="2" class="vide"><a href="javascript:history.go(-1)"><img src="images/infos/retour.png" alt=""></a></td>
+						<td class="vide"><a href="javascript:history.go(-1)"><img src="images/infos/retour.png" alt=""></a></td>
 						<td><img src="images/infos/chapeau.png" alt=""></td>
 						<td><img src="images/infos/cafe.png" alt=""></td>
 					</tr>
 					<tr>
-						<!-- <td></td> -->
+						<td><a href="php/deconnect.php"><img src="images/infos/quit.png" alt=""></a></td>
 						<td>14/20</td>
 						<td>0 café</td>
 					</tr>
 				</tbody>
 			</table>
-
 		</div>
-		
-	<div class="duel">
 
-		<div class="titrecours">Que le duel commence!</div>
+		<div class="duel">
+		<h2>Attaques</h2>
+			<table>
+				<tr>
+					<td><a href=""><img src="images/salledeclasse/attaque.png" alt="" class="attaque"></a></td>
+					<td><a href=""><img src="images/salledeclasse/attaque.png" alt="" class="attaque"></a></td>
+					<td><a href=""><img src="images/salledeclasse/attaque.png" alt="" class="attaque"></a></td>
+					<td><a href=""><img src="images/salledeclasse/attaque.png" alt="" class="attaque"></a></td>
+				</tr>
+				<tr>
+					<td><p>Dragon de feu</p></td>
+					<td><p>Div DTC</p></td>
+					<td><p>Plume</p></td>
+					<td><p>pouêt</p></td>
+				</tr>
+			</table>
+		</div>
 
-		<img src="images/salledeclasse/prof-php.png" alt="prof" class="prof">
-
-	</div>
 </body>
 </html>
